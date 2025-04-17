05ceb97655551eb2834b5097c0c24dd3231e32d5
loty <ez276182@dal.ca>
2025-04-16 21:00:03 -0300

    build ready commit

--- src/main/java/com/tottstats/spursstats/WebConfig.java
+++ src/main/java/com/tottstats/spursstats/WebConfig.java
@@ -1,5 +1,6 @@
 package com.tottstats.spursstats;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")  // Apply to all endpoints
-                .allowedOrigins("http://localhost:3000")  // React app URL
+                .allowedOrigins(frontendUrl)  // React app URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed HTTP methods
                 .allowedHeaders("*");  // Allow any headers
     }
